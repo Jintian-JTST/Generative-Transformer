5d74f86b7279ffccd5b1b401b1b4772a88ee8bd9
Jintian Wang <wangjintianlovemc@163.com>
2026-01-25 21:31:37 +0000

    Encoder and decoder

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ chars=sorted(list(set(content)))
 vocab_size=len(chars)
 print("Vocab size:", vocab_size)
 print("Characters:", ''.join(chars))
+
+# Create mappings from characters to integers and vice versa
+stoi={ch:i for i,ch in enumerate(chars)}
+itos={i:ch for i,ch in enumerate(chars)}
+def encode(s):
+    return [stoi[c] for c in s]
+def decode(l):
+    return ''.join(itos[i] for i in l)
+
+print(encode("hello world"))
+print(decode(encode("hello world")))
